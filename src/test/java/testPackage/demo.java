a67e4ffd6088e8bbf158657faf26a4c51cab2038
Mrinmoy Kumar Biswas <mrinmoy.blr@gmail.com>
2021-07-03 14:16:33 +0530

    Final Commit from Asian Development Team

---
+++ src/test/java/testPackage/demo.java
@@ -0,0 +1,14 @@
+package testPackage;
+
+import org.testng.annotations.Test;
+
+public class demo {
+	
+	@Test
+	public void AppiumTestMethods33() {
+		System.out.println("AppiumTestMethods");
+		
+	}
+	
+
+}
