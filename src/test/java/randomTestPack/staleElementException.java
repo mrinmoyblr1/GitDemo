7b819f584e091734a487f2ef2a34893f6584f1e2
Mrinmoy Kumar Biswas <mrinmoy.blr@gmail.com>
2021-07-04 09:33:34 +0530

    From Mrinmoy Biswas

--- src/test/java/randomTestPack/staleElementException.java
+++ src/test/java/randomTestPack/staleElementException.java
@@ -9,6 +9,7 @@ public class staleElementException {
 	public void staleElement()
 	{
 		System.out.println("Mrinmoy Biswas=============================");
+		System.out.println("Kumar===============");
 	}
 	
 
